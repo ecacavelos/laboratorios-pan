e097a26ae1e4b21a3199e47020ef0ee8d695487b
estebancacavelos <estebancacavelos@f0c7e29c-ecaf-6458-6451-c4aa5355ce14>
2012-05-23 15:26:03 +0000

    Se incrusto la galeria de imagenes galleriffic (example 2) en la seccion de productos y se modifico el layout por default para que se incluyan los .js y los .css de la galeria de imagenes.

---
+++ trunk/PHP-VERSION/app/Controller/ProductosController.php
@@ -0,0 +1,13 @@
+<?php
+class ProductosController extends AppController {
+    
+	public $helpers = array('Html', 'Form');
+	public $name = 'Productos';
+	
+	public function index() {
+		
+	 //$this->set('productos', $this->Producto->find('all'));
+	}
+	
+	
+}
